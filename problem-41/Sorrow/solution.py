847aeed7751f2c66c0765bf4ecb59d30be097436
Carl Gherardi <carl.gherardi@gmail.com>
2011-06-08 18:29:20 +0800

    Problem 41 solution
    Once I click that pandigitals could be generated using permutations...
    - Gah - no 10 digit panditials - 0 doesn't count
    - Gah +1 pandigital definition only includes 1->n
    - Gah!! no 8 or 9 digit pandigitals

--- problem-41/Sorrow/solution.py
+++ problem-41/Sorrow/solution.py
@@ -7,41 +7,52 @@
 #
 #What is the largest n-digit pandigital prime that exists?
 
+import time
+start = time.time()
+
 import itertools
+import math
 
 VERBOSE = 1
 
-def is_pandigital(n):
-    pandigital = True
-    tmp = {}
-    for c in n:
-        if c in tmp:
-            pandigital = False
-            break
-        else:
-            tmp[c] = True
-    return pandigital        
-
-MAX = 50000000
-n = 6
+sieve_exists = False
+sieve_size = int(math.sqrt(9999999))
 sieve = {}
 primes = [2]
-val = 0
 
-for i in itertools.count(start=3):
-    if i > MAX:
-        break
-    if i%2 == 0:
-        pass
-    else:
-        if i not in sieve:
-            if is_pandigital(str(i)):
-                val = i
-                print val
-            j = i
-            while j < MAX:
-                sieve[j] = True # setdefault?
-                j +=i
+def create_sieve():
+    print "Building sieve size: %s" % sieve_size
+    for i in itertools.count(start=3):
+        if i > sieve_size:
+            break
+        if i%2 == 0:
+            pass
+        else:
+            if i not in sieve:
+                primes.append(i)
+                j = i
+                while j < sieve_size:
+                    sieve[j] = True # setdefault?
+                    j +=i
+    print "Sieve completed"
 
-print val
+def is_prime(n):
+    global sieve_exists
+    if not sieve_exists:
+        create_sieve()
+        sieve_exists = True
+    # Test precalculated primes up to 10M
+    for i in primes:
+        if n % i == 0:
+            return False
 
+    return True
+
+for i in itertools.permutations("7654321", 7):
+    n = int("%s%s%s%s%s%s%s" % (i[0],i[1],i[2],i[3],i[4],i[5],i[6]))
+    if is_prime(n):
+        print n
+        exit()
+
+end = time.time() - start
+print "Time Taken: " + str(end) + "ms"
