8cb72bb6d4a52f2f933c87a4ad69f245c36d6b37
Carl Gherardi <carl.gherardi@gmail.com>
2011-06-09 13:45:37 +0800

    Problem 24 solution

---
+++ problem-24/Sorrow/solution.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Problem 24
+
+# A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+# 
+# 012   021   102   120   201   210
+# 
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+
+import time
+start = time.time()
+
+import itertools
+
+VERBOSE = 0
+
+count = 0
+
+for i in itertools.permutations("0123456789", 10):
+    count += 1
+    if count == 1000000:
+        print i
+        break
+
+end = time.time() - start
+print "Time Taken: " + str(end) + "ms"
+
