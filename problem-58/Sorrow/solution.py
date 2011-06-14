98359944ec7426147544227e6ba33f334396c73e
Carl Gherardi <carl.gherardi@gmail.com>
2011-06-14 19:25:28 +0800

    Problem 58 solution
    Did homework and went back to an earlier problems comments

--- problem-58/Sorrow/solution.py
+++ problem-58/Sorrow/solution.py
@@ -10,60 +10,36 @@ start = time.time()
 
 VERBOSE = 1
 
-import itertools
-import math
 import gmpy
 
 sieve = {2:True}
 primes = [2]
 
 def is_prime(n):
-    return gmpy.is_prime(n)
+    if gmpy.is_prime(n) > 0:
+        return True
+    return False
 
-def generate_spiral():
-    MIN = 10000000
-    number = 2
-    leng = 1
-    vec = 1
-    while True:
-        for i in range(1, leng + 1):
-            number += 1
-        yield (number -1)
-        for j in range(1, leng + 1):
-            number += 1
-        yield (number -1)
-        leng += 1
+def generate_diags(n):
+    """n^2-n+1     n^2
+       n^2-2n+2    n^2-3n+3
+       Solution from problem 28 comments"""
+    return [n*n-n+1, n*n, n*n-(2*n)+2, n*n-(3*n)+3]
 
-def get_prime_ratio(lst):
-    den = len(lst)
-    num = []
-    for x in lst[:-4]:
-        if is_prime(x):
-            num.append(x)
-    #print "%s/%s: %s" %(len(num), den, float(len(num))/float(den))
-    #print num
-    return float(len(num))/float(den)
+primes = 0
+x = 3
 
-ratio = 1
+while True:
+    tmp = generate_diags(x)
+    for i in tmp:
+        if is_prime(i):
+            primes += 1
+    den = 2*x-1
+    if float(primes)/float(den) < 0.10:
+        print "Solution: %s" % x
+        break
+    x += 2
 
-size = 1
-
-diags = []
-
-count = 0
-for x in generate_spiral():
-    diags.append(x)
-    count = count % 4
-    if count == 0:
-        if len(diags) > 10000:
-            ratio = get_prime_ratio(diags)
-            if (size % 50) - 1 == 0:
-                print "(%s): %s" %(size, ratio)
-        size += 2
-        if ratio < 0.1 and len(diags) > 1:
-            print "(%s): %s" %(size, ratio)
-            break
-    count += 1
 
 end = time.time() - start
 print "Time Taken: " + str(end) + "ms"
